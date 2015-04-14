886f36eed030b61fc94de4f98c21837f52c70901
Jose Zamudio <zamudio@outlook.com>
2015-04-14 01:29:11 -0700

    Add dynamic divs 3-5

--- main.js
+++ main.js
@@ -2,34 +2,37 @@ $(document).ready(init);
 
 var $source = undefined, $target = undefined, count = 0;
 
-function init(){
-  $('#1').text('1').css('width', '20%');
-  $('#2').text('2').css('width', '40%');
-  $('#3').text('3').css('width', '60%');
-
-  $('.disc').click(selectDisk);
+function init() {
+  $('#start').click(startBoard);
+  $('.tower').on('click', '.disc', selectDisk);
   $('.tower').click(dropTower);
+}
 
-  function selectDisk() {
-    event.stopPropagation();
-    $(this).is(':first-child') ? $source = $(this) : alert('Illegal move');
+function startBoard() {
+  $('#tower1').empty();
+  for (var i = 0; i < $('#number').val(); i++) {
+    var $div = $('<div>');
+    $div.attr('id', i+1).addClass('disc').css('width', (((i+1)/10)*100) + '%').text(i+1);
+    $('#tower1').append($div);
   }
+}
 
-  function dropTower() {
-    $target = $(this);
-    moveDisk();
-  }
+function selectDisk() {
+  event.stopPropagation();
+  $(this).is(':first-child') ? $source = $(this) : alert('Illegal move');
+}
 
-  function moveDisk() {
-    var children = $target.children('.disc')[0];
-    (children && $source.attr('id') > children.id) ? alert('Illegal move') : $target.prepend($source);
-    count++;
-    win();
-  }
+function dropTower() {
+  $target = $(this);
+  moveDisk();
+}
 
-  function win() {
-    if ($('#tower3').children().length === 3) {
-      alert('YOU WON!');
-    }
-  }
+function moveDisk() {
+  var children = $target.children('.disc')[0];
+  (children && $source.attr('id') > children.id) ? alert('Illegal move') : $target.prepend($source);
+  win();
+}
+
+function win() {
+  if ($('#tower3').children().length === 3) { alert('YOU WON!'); }
 }
